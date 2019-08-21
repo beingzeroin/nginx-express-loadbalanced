802bd6c43ad35514873923e8f3176561989837a0
Sandeep <Sandeep>
2019-08-21 18:12:28 +0530

    Enhanced through AutoPolling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const path = require('path');
 const winston = require('winston');
+const os = require('os');
 
 const app = express();
+
+const appPort = process.env.PORT || 3000
+const nodeenv = process.env.NODE_ENV || 'development'
+const instanceName = process.env.INSTANCE || os.hostname()
+
+const environment = {
+  title: 'Docker with Nginx and Express',
+  node: nodeenv,
+  instance: instanceName,
+  port: appPort
+};
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -10,18 +22,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  const environment = {
-    title: 'Docker with Nginx and Express',
-    node: process.env.NODE_ENV,
-    instance: process.env.INSTANCE,
-    port: process.env.PORT
-  };
-
   res.render('index', { environment });
 });
 
-app.listen(process.env.PORT, () => {
-  winston.info(`NODE_ENV: ${process.env.NODE_ENV}`);
-  winston.info(`INSTANCE: ${process.env.INSTANCE}`);
-  winston.info(`EXPRESS: ${process.env.PORT}`);
+app.get('/data', (req, res) => {
+  res.json(environment);  
+});
+
+app.listen(appPort, () => {
+  winston.info(`NODE_ENV: ${nodeenv}`);
+  winston.info(`INSTANCE: ${instanceName}`);
+  winston.info(`EXPRESS: ${appPort}`);
 });
---
+++ src/public/js/site.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    var reqCounter= 1;
+
+    function poll() 
+    {
+        $.ajax({
+            url: "/data",
+            type: "GET",
+            success: function(data) {
+                console.log(JSON.stringify(data));
+                $("#requestid").html(reqCounter); 
+                $("#environment").html(data.node); 
+                $("#instance").html(data.instance); 
+                $("#port").html(data.port); 
+                reqCounter = reqCounter + 1
+            },
+            dataType: "json",
+            complete: setTimeout(function() {poll()}, 1000),
+            timeout: 1000
+        })
+    }
+    poll();
+});
